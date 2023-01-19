ddc5b278144d7c854e4a174d793452f1990a1c11
Maryan <maryanay2000@gmail.com>
2023-01-19 16:02:30 -0500

    Adding files

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,12 +1,22 @@
 const request = require('request');
 const breedName = process.argv[2];
 
-request('https://api.thecatapi.com/v1/breedsl/search ?q=' + breedName, (error, response, body) => {
-  console.log('Breed is not found:', error); // Print the error if one occurred
+request('https://api.thecatapi.com/v1/breeds/search?q=' + breedName, (error, response, body) => {
+
+  // Print the error if one occurred
+  if (error) {
+    console.log(error);
+    return;
+  }
   //console.log('statusCode:', response && response.statusCode);
   //console.log('Type of body:', typeof body);
   const data = JSON.parse(body);
   const breed = data[0];
+  if (!breed) {
+    console.log("Breed not found");
+    return;
+  }
   console.log(breed.description);
-  //console.log("Type of body:", typeof data);
 });
+
+
