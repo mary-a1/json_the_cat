8fe29b6185ea455aed6739ec82efc419359162fd
Maryan <maryanay2000@gmail.com>
2023-01-19 16:59:43 -0500

    Refractored code and added test

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,22 +1,25 @@
 const request = require('request');
 const breedName = process.argv[2];
 
-request('https://api.thecatapi.com/v1/breeds/search?q=' + breedName, (error, response, body) => {
 
-  // Print the error if one occurred
-  if (error) {
-    console.log(error);
-    return;
-  }
-  //console.log('statusCode:', response && response.statusCode);
-  //console.log('Type of body:', typeof body);
-  const data = JSON.parse(body);
-  const breed = data[0];
-  if (!breed) {
-    console.log("Breed not found");
-    return;
-  }
-  console.log(breed.description);
-});
+const fetchBreedDescription = function(breedName, callback) {
+  request('https://api.thecatapi.com/v1/breeds/search?q=' + breedName, (error, response, body) => {
 
+    // Print the error if one occurred
+    if (error) {
+      callback(error, null);
+      return;
+    }
 
+    const [data] = JSON.parse(body);
+    
+    if (!data) {
+      return callback(error, null);
+      
+    }
+    return callback(null, data.description);
+
+  });
+};
+
+module.exports = { fetchBreedDescription };
